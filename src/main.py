af6f32e02280c057ff662b4682e72ee832ebc0d8
igor <gritsyuk.igor@gmail.com>
2024-03-03 00:39:39 +0300

    path logging

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="../log/bot_ballons.log",
+    logging.basicConfig(filename="/app/log/bot_ballons.log",
                         level=logging.INFO,
                         format="%(asctime)s %(name)s %(levelname)s %(message)s",
                         )
